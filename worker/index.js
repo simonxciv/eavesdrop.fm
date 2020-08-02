a2bc4c84e8dace3be5fb41a3e401c20a33764173
Simon Buckley <hello@simonbuckley.com.au>
2020-08-02 13:22:57 +1000

    formatting fix

--- worker/index.js
+++ worker/index.js
@@ -7,50 +7,50 @@ addEventListener('fetch', event => {
 * @param {Request} request
 */
 async function handleRequest(request) {
-// We only care about POSTs. Pass everything else straight through to the origin
-if(request.method !== 'POST') {
-  return await fetch(request)
-}
+  // We only care about POSTs. Pass everything else straight through to the origin
+  if(request.method !== 'POST') {
+    return await fetch(request)
+  }
 
-// store the request body
-let body = await request.formData()
-body = await JSON.parse(body.get('payload'));
-console.log(body)
-// store an object of parameters
-const params = await getParams(request)
-// an array of events we actually care about. we'll ignore everything else
-const acceptedEvents = ['media.scrobble', 'media.play','media.resume', 'media.listen']
+  // store the request body
+  let body = await request.formData()
+  body = await JSON.parse(body.get('payload'));
+  console.log(body)
+  // store an object of parameters
+  const params = await getParams(request)
+  // an array of events we actually care about. we'll ignore everything else
+  const acceptedEvents = ['media.scrobble', 'media.play','media.resume', 'media.listen']
 
-if(body.Metadata.type !== 'track') {
-  return new Response('Not a track!', { status: 200})
-}
+  if(body.Metadata.type !== 'track') {
+    return new Response('Not a track!', { status: 200})
+  }
 
-// Ignore the request if it's not in our list of accepted events
-if (!acceptedEvents.includes(body.event)) {
-  return new Response('Nothing to do', { status: 200 })
-}
+  // Ignore the request if it's not in our list of accepted events
+  if (!acceptedEvents.includes(body.event)) {
+    return new Response('Nothing to do', { status: 200 })
+  }
 
-// If the username from Plex doesn't match the username in the query string
-if (body.Account.title.toLowerCase() !== params.user.toLowerCase()) {
-  return new Response('Plex user does not match query string', { status: 200 })
-}
+  // If the username from Plex doesn't match the username in the query string
+  if (body.Account.title.toLowerCase() !== params.user.toLowerCase()) {
+    return new Response('Plex user does not match query string', { status: 200 })
+  }
 
-// Send the user's ListenBrainz token to LB for validation, and reject the request if it's invalid
-try {
-  await validateUser(params.id)
-} catch (error) {
-  return new Response(error, { status: 500 })
-}
+  // Send the user's ListenBrainz token to LB for validation, and reject the request if it's invalid
+  try {
+    await validateUser(params.id)
+  } catch (error) {
+    return new Response(error, { status: 500 })
+  }
 
-// Submit the event to ListenBrainz
-try {
-  await submitListen(generateObject(body),params.id)
-} catch (error) {
-  return new Response(error, { status: 500 })
-}
+  // Submit the event to ListenBrainz
+  try {
+    await submitListen(generateObject(body),params.id)
+  } catch (error) {
+    return new Response(error, { status: 500 })
+  }
 
-// And finally, return our success message :)
-return new Response('success', { status: 200 })
+  // And finally, return our success message :)
+  return new Response('success', { status: 200 })
 }
 
 /**
@@ -58,18 +58,18 @@ return new Response('success', { status: 200 })
 * @param {Request} request
 */
 async function getParams(request) {
-let params = {}
-const url = new URL(request.url)
-const queryString = url.search.slice(1).split('&')
-queryString.forEach(item => {
-  const [key, value] = item.split('=')
-  if(key === 'id') {
-    params['id'] = value
-  } else if(key === 'user') {
-    params['user'] = value
-  }
-})
-return params
+  let params = {}
+  const url = new URL(request.url)
+  const queryString = url.search.slice(1).split('&')
+  queryString.forEach(item => {
+    const [key, value] = item.split('=')
+    if(key === 'id') {
+      params['id'] = value
+    } else if(key === 'user') {
+      params['user'] = value
+    }
+  })
+  return params
 }
 
 /**
@@ -77,20 +77,20 @@ return params
 * @param {String} listenBrainzToken
 */
 async function validateUser(listenBrainzToken) {
-return await fetch('https://api.listenbrainz.org/1/validate-token?token=' + listenBrainzToken)
-.then(response => {
-  if(!response.ok) {
-    throw Error('Something went wrong when we tried looking up the ListenBrainz token')
-  }
-  return response.json()
-}).then(responseJson => {
-  if(responseJson.message !== 'Token valid.') {
-    throw Error('ListenBrainz token is not valid')
-  }
-  return responseJson
-}).catch(error => {
-  throw error
-})
+  return await fetch('https://api.listenbrainz.org/1/validate-token?token=' + listenBrainzToken)
+  .then(response => {
+    if(!response.ok) {
+      throw Error('Something went wrong when we tried looking up the ListenBrainz token')
+    }
+    return response.json()
+  }).then(responseJson => {
+    if(responseJson.message !== 'Token valid.') {
+      throw Error('ListenBrainz token is not valid')
+    }
+    return responseJson
+  }).catch(error => {
+    throw error
+  })
 }
 
 /**
@@ -98,27 +98,27 @@ return await fetch('https://api.listenbrainz.org/1/validate-token?token=' + list
 * @param {Object} body
 */
 function generateObject(body) {
-const now = new Date()
-const timestamp = Math.round(now.getTime() / 1000)
-const object = {
-  "listen_type": body.event === 'media.scrobble' ? 'single' : 'playing_now',
-  "payload": [
-    {
-      "track_metadata": {
-        "additional_info": {
-          "listening_from": "Plex",
-        },
-        "artist_name": body.Metadata.grandparentTitle,
-        "track_name": body.Metadata.title,
-        "release_name": body.Metadata.parentTitle
+  const now = new Date()
+  const timestamp = Math.round(now.getTime() / 1000)
+  const object = {
+    "listen_type": body.event === 'media.scrobble' ? 'single' : 'playing_now',
+    "payload": [
+      {
+        "track_metadata": {
+          "additional_info": {
+            "listening_from": "Plex",
+          },
+          "artist_name": body.Metadata.grandparentTitle,
+          "track_name": body.Metadata.title,
+          "release_name": body.Metadata.parentTitle
+        }
       }
-    }
-  ]
-}
-if (body.event === 'media.scrobble') {
-  object.payload[0].listened_at = timestamp
-}
-return JSON.stringify(object)
+    ]
+  }
+  if (body.event === 'media.scrobble') {
+    object.payload[0].listened_at = timestamp
+  }
+  return JSON.stringify(object)
 }
 
 /**
