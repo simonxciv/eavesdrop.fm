c4b1a3c894d86c924f522dac3aa503b117d2ce03
Daniel THIRION <defvs.daniel@gmail.com>
2024-06-26 16:43:08 +0200

    Use const, move track_mbid to additional_info

--- src/lib/helpers/generateListenbrainzBody.ts
+++ src/lib/helpers/generateListenbrainzBody.ts
@@ -2,7 +2,7 @@ import type Payload from '$lib/typing/payload';
 
 // generate the body required by LB: https://listenbrainz.readthedocs.io/en/latest/dev/json/#submission-json
 const generateListenbrainzBody = (body: Payload) => {
-	let track_mbid = body.Metadata?.Guid?.find((v) => v.id?.startsWith("mbid://"))?.id?.substring(7);
+	const track_mbid = body.Metadata?.Guid?.find((v) => v.id?.startsWith("mbid://"))?.id?.substring(7);
 	return JSON.stringify({
 		listen_type: body.event === 'media.scrobble' ? 'single' : 'playing_now',
 		payload: [
@@ -10,12 +10,12 @@ const generateListenbrainzBody = (body: Payload) => {
 				listened_at: body.event === 'media.scrobble' ? Math.floor(Date.now() / 1000) : undefined,
 				track_metadata: {
 					additional_info: {
-						listening_from: 'Plex'
+						listening_from: 'Plex',
+					  ...(track_mbid !== undefined && { track_mbid: track_mbid })
 					},
 					artist_name: body.Metadata.originalTitle ?? body.Metadata.grandparentTitle,
 					track_name: body.Metadata.title,
 					release_name: body.Metadata.parentTitle,
-					...(track_mbid !== undefined && { track_mbid: track_mbid })
 				}
 			}
 		]
